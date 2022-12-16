6b7e8b0a1cb4aee5468be38b4811d44dd773c018
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2022-12-16 18:37:01 +0700

    feat: added test connection to db

--- src/server/trpc/router/example.ts
+++ src/server/trpc/router/example.ts
@@ -5,7 +5,11 @@ import { router, publicProcedure } from "../trpc";
 export const exampleRouter = router({
   hello: publicProcedure
     .input(z.object({ text: z.string().nullish() }).nullish())
-    .query(({ input }) => {
+    .query(async ({ input, ctx }) => {
+      ctx.prisma.$connect().then(() => {
+        console.log("================= CONNECTED TO DB ================");
+      });
+
       return {
         greeting: `Hello ${input?.text ?? "world"}`,
       };
