c1016dcfae4616425e7ca89745d05d0e43f3df2c
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2022-12-18 00:49:46 +0700

    chore: removed unused code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -58,16 +58,6 @@ const Home: NextPage = () => {
         />
       </Head>
       {renderCampaigns()}
-      <Campaign
-        slug={"testing-slug"}
-        endDate={moment("2022-12-")}
-        accuAmount={500_000}
-        targetAmount={1_000_000}
-        title="Bantu para penerus bangsa melanjutkan pendidikan"
-        id={1}
-        shortDescription="This is a short description. Lorem ipsum, dolor sit amet consectetur adipisicing elit. At, est nostrum sit harum nobis inventore explicabo reprehenderit rerum amet incidunt!"
-        imageUrl="https://images.unsplash.com/photo-1527004013197-933c4bb611b3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80"
-      />
     </Box>
   );
 };
