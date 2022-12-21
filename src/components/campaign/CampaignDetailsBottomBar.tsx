2a6ccf8e15aead8db925a3efd01a8ae6f9b7f56d
theodevoid <87703322+theodevoid@users.noreply.github.com>
2022-12-21 20:34:10 +0700

    refactor: made donation modal reusable

--- src/components/campaign/CampaignDetailsBottomBar.tsx
+++ src/components/campaign/CampaignDetailsBottomBar.tsx
@@ -1,50 +1,10 @@
-import {
-  Box,
-  Text,
-  Button,
-  Container,
-  Modal,
-  Input,
-  Textarea,
-  Checkbox,
-} from "@mantine/core";
+import { Box, Button, Container } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import DonationModal from "components/donation/DonationModal";
 import { BAR_HEIGHT } from "constants/styles";
-import { useFormik } from "formik";
-import * as yup from "yup";
 
-// TODO: Make modal reusable
 const CampaignDetailsBottomBar = ({ campaignId }: { campaignId: string }) => {
   const [opened, handlers] = useDisclosure(false);
-
-  const formik = useFormik({
-    initialValues: {
-      amount: 0,
-      message: "",
-    },
-    onSubmit: async () => {
-      return;
-    },
-    validationSchema: yup.object().shape({
-      amount: yup
-        .number()
-        .required()
-        .min(10_000, "Minimum pemberian Rp. 10.000")
-        .max(100_000_000, "Maximum pemberian Rp. 100.000.000"),
-    }),
-  });
-
-  const handleNominalInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { target } = event;
-    const { value } = target;
-
-    if (value) {
-      formik.setFieldValue("amount", parseInt(value.replace(/[^0-9]+/g, "")));
-      return;
-    }
-
-    formik.setFieldValue("amount", 0);
-  };
 
   return (
     <>
@@ -68,50 +28,11 @@ const CampaignDetailsBottomBar = ({ campaignId }: { campaignId: string }) => {
           </Button>
         </Box>
       </Container>
-      <Modal
-        opened={opened}
+      <DonationModal
+        campaignId={campaignId}
         onClose={handlers.close}
-        title="Berikan bantuan"
-        styles={{
-          title: {
-            fontWeight: 500,
-          },
-        }}
-      >
-        <Input.Wrapper
-          label="Nominal Donasi"
-          withAsterisk
-          error={formik.errors.amount}
-        >
-          <Input
-            size="xl"
-            icon={<Text size="lg">Rp.</Text>}
-            placeholder={(10000).toLocaleString("id-ID")}
-            onChange={handleNominalInput}
-            value={formik.values.amount.toLocaleString("id-ID")}
-          />
-        </Input.Wrapper>
-        <Input.Wrapper
-          label="Tulis sebuah pesan"
-          mt="md"
-        >
-          <Textarea
-            size="md"
-            minRows={3}
-          />
-        </Input.Wrapper>
-        <Checkbox
-          label="Berikan bantuan secara anonim"
-          mt="md"
-        />
-        <Button
-          fullWidth
-          mt="lg"
-          onClick={() => formik.handleSubmit}
-        >
-          Lanjutkan pembayaran
-        </Button>
-      </Modal>
+        opened={opened}
+      />
     </>
   );
 };
