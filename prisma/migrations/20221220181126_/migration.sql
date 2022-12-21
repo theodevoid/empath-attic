58bab804d3cb6aaf441925bae6ecf2e7727a43f8
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2022-12-22 00:08:10 +0700

    fix(db): removed all migrations

--- prisma/migrations/20221220181126_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `short_description` on the `Campaign` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(140)`.
-
-*/
--- AlterTable
-ALTER TABLE "Campaign" ALTER COLUMN "short_description" SET DATA TYPE VARCHAR(140);
