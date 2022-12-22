bc179ad326005039f1f3cbc9d0dc8fbad9dd1e55
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2022-12-22 14:42:48 +0700

    feat(xendit): added create invoice function

---
+++ src/server/lib/xendit.ts
@@ -0,0 +1,27 @@
+import Xendit from "xendit-node";
+import { env } from "env/server.mjs";
+
+const x = new Xendit({
+  secretKey: env.XENDIT_SERVER_KEY,
+});
+const i = new x.Invoice({});
+
+export const createXenditInvoice = async (
+  paymentId: string,
+  amount: number,
+) => {
+  try {
+    const res: any = await i.createInvoice({
+      externalID: paymentId,
+      amount,
+      invoiceDuration: 3600, // 1 hour
+    });
+
+    return {
+      amount,
+      invoiceUrl: res.invoice_url,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
