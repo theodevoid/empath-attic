877e58ef0f5cab35247d6399cde909cc6d321102
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2022-12-22 16:52:46 +0700

    feat(donation): submit donation feature

--- src/components/donation/DonationModal.tsx
+++ src/components/donation/DonationModal.tsx
@@ -1,6 +1,55 @@
-import { Text, Button, Modal, Input, Textarea, Checkbox } from "@mantine/core";
+import {
+  Text,
+  Button,
+  Modal,
+  Input,
+  Textarea,
+  Checkbox,
+  Loader,
+  Box,
+  Alert,
+  Anchor,
+  Flex,
+} from "@mantine/core";
 import { useFormik } from "formik";
+import { useEffect } from "react";
+import { MdCheck } from "react-icons/md";
+import { trpc } from "utils/trpc";
 import * as yup from "yup";
+
+const AfterSubmitState = ({ invoiceUrl }: { invoiceUrl: string }) => {
+  return (
+    <Box>
+      <Alert
+        icon={<MdCheck />}
+        title="Pembayaran berhasil terbuat!"
+        color="green"
+      >
+        Yey! Pembayaran kamu sudah sukses terbuat. Selesaikan pembayaran untuk
+        menyelesaikan proses donasi. Terima kasih atas bantuannya sahabat
+        berempati!
+      </Alert>
+      <Text
+        size="sm"
+        align="center"
+        mt="md"
+      >
+        Klik link di bawah ini jika layar kamu tidak dipindahkan
+      </Text>
+      <Flex justify="center">
+        <Anchor
+          align="center"
+          size="sm"
+          mt={4}
+          href={invoiceUrl}
+          target="_blank"
+        >
+          {invoiceUrl.slice(0, 50)}
+        </Anchor>
+      </Flex>
+    </Box>
+  );
+};
 
 type DonationModalProps = {
   opened: boolean;
@@ -8,14 +57,30 @@ type DonationModalProps = {
   campaignId: string;
 };
 
-const DonationModal = ({ opened, onClose }: DonationModalProps) => {
+// TODO: cannot open if user is not logged in
+const DonationModal = ({ opened, onClose, campaignId }: DonationModalProps) => {
+  const {
+    isLoading,
+    mutate: handleSubmitDonation,
+    data,
+    isSuccess,
+    reset,
+  } = trpc.campaign.donateToCampaign.useMutation();
+
   const formik = useFormik({
     initialValues: {
       amount: 0,
       message: "",
+      isAnonymous: false,
     },
-    onSubmit: async () => {
-      return;
+    onSubmit: async values => {
+      const { amount, message } = values;
+
+      handleSubmitDonation({ amount, message, campaignId });
+
+      formik.setFieldValue("amount", 0);
+      formik.setFieldValue("message", "");
+      formik.setFieldValue("isAnonymous", false);
     },
     validationSchema: yup.object().shape({
       amount: yup
@@ -23,6 +88,8 @@ const DonationModal = ({ opened, onClose }: DonationModalProps) => {
         .required()
         .min(10_000, "Minimum pemberian Rp. 10.000")
         .max(100_000_000, "Maximum pemberian Rp. 100.000.000"),
+      message: yup.string().nullable(),
+      isAnonymouse: yup.boolean(),
     }),
   });
 
@@ -38,6 +105,36 @@ const DonationModal = ({ opened, onClose }: DonationModalProps) => {
     formik.setFieldValue("amount", 0);
   };
 
+  const handleMessageInput = (
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    const { target } = event;
+    const { value } = target;
+
+    if (value) {
+      formik.setFieldValue("message", value);
+      return;
+    }
+  };
+
+  const handleCheckbox = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { checked } = event.currentTarget;
+
+    formik.setFieldValue("isAnonymous", checked);
+  };
+
+  useEffect(() => {
+    if (!opened) {
+      reset();
+    }
+  }, [opened]);
+
+  useEffect(() => {
+    if (isSuccess) {
+      window.open(data?.payment_gateway_invoice_url as string);
+    }
+  }, [isSuccess, data?.payment_gateway_invoice_url]);
+
   return (
     <Modal
       opened={opened}
@@ -49,39 +146,51 @@ const DonationModal = ({ opened, onClose }: DonationModalProps) => {
         },
       }}
     >
-      <Input.Wrapper
-        label="Nominal Donasi"
-        withAsterisk
-        error={formik.errors.amount}
-      >
-        <Input
-          size="xl"
-          icon={<Text size="lg">Rp.</Text>}
-          placeholder={(10000).toLocaleString("id-ID")}
-          onChange={handleNominalInput}
-          value={formik.values.amount.toLocaleString("id-ID")}
+      {isSuccess ? (
+        <AfterSubmitState
+          invoiceUrl={data?.payment_gateway_invoice_url as string}
         />
-      </Input.Wrapper>
-      <Input.Wrapper
-        label="Tulis sebuah pesan"
-        mt="md"
-      >
-        <Textarea
-          size="md"
-          minRows={3}
-        />
-      </Input.Wrapper>
-      <Checkbox
-        label="Berikan bantuan secara anonim"
-        mt="md"
-      />
-      <Button
-        fullWidth
-        mt="lg"
-        onClick={() => formik.handleSubmit}
-      >
-        Lanjutkan pembayaran
-      </Button>
+      ) : (
+        <>
+          <Input.Wrapper
+            label="Nominal Donasi"
+            withAsterisk
+            error={formik.errors.amount}
+          >
+            <Input
+              size="xl"
+              icon={<Text size="lg">Rp.</Text>}
+              placeholder={(10000).toLocaleString("id-ID")}
+              onChange={handleNominalInput}
+              value={formik.values.amount.toLocaleString("id-ID")}
+            />
+          </Input.Wrapper>
+          <Input.Wrapper
+            label="Tulis sebuah pesan"
+            mt="md"
+          >
+            <Textarea
+              size="md"
+              minRows={3}
+              onChange={handleMessageInput}
+            />
+          </Input.Wrapper>
+          <Checkbox
+            label="Berikan bantuan secara anonim"
+            mt="md"
+            onChange={handleCheckbox}
+          />
+          {formik.isValid}
+          <Button
+            fullWidth
+            mt="lg"
+            onClick={() => formik.handleSubmit()}
+            disabled={isLoading}
+          >
+            {isLoading ? <Loader /> : "Lanjutkan pembayaran"}
+          </Button>
+        </>
+      )}
     </Modal>
   );
 };
--- src/server/trpc/router/campaign.ts
+++ src/server/trpc/router/campaign.ts
@@ -1,4 +1,6 @@
-import { router, publicProcedure } from "../trpc";
+import { createXenditInvoice } from "server/lib/xendit";
+import { z } from "zod";
+import { router, publicProcedure, protectedProcedure } from "../trpc";
 
 export const campaignRouter = router({
   getAll: publicProcedure.query(({ ctx }) => {
@@ -10,4 +12,58 @@ export const campaignRouter = router({
 
     return findAllCampaigns;
   }),
+  donateToCampaign: protectedProcedure
+    .input(
+      z.object({
+        campaignId: z.string(),
+        amount: z.number(),
+        message: z.string().nullish(),
+        isAnonymous: z.boolean().nullish(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      // 1. create CampaignDonation
+      // 2. create xendit invoice
+      // 3. update CampaignDonation with xendit invoice ID
+      const { prisma, session } = ctx;
+      const { amount, campaignId, message, isAnonymous } = input;
+      const { user } = session;
+
+      const donationTransaction = await prisma.$transaction(async tx => {
+        const createDonation = await tx.campaignDonation.create({
+          data: {
+            amount,
+            campaign_id: campaignId,
+            message,
+            is_anon: isAnonymous ?? false,
+            user_id: user.id,
+          },
+        });
+
+        const xenditInvoice = await createXenditInvoice(
+          createDonation.id,
+          amount,
+        );
+
+        console.log(xenditInvoice);
+
+        if (!xenditInvoice?.invoiceId) {
+          throw new Error("Failed to create Xendit invoice");
+        }
+
+        const updatedDonation = await tx.campaignDonation.update({
+          data: {
+            payment_gateway_invoice_id: xenditInvoice.invoiceId,
+            payment_gateway_invoice_url: xenditInvoice.invoiceUrl,
+          },
+          where: {
+            id: createDonation.id,
+          },
+        });
+
+        return updatedDonation;
+      });
+
+      return donationTransaction;
+    }),
 });
