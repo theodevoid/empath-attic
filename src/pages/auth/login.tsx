b7770b120a692e75cc3f99f8871ba275855415cb
Theodore Rivaldo Devo Mangowal <87703322+theodevoid@users.noreply.github.com>
2022-12-28 16:06:07 +0700

    [REF] fixed layouts and refactored things (#12)

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,10 +1,12 @@
 import { Stack, Title, Button } from "@mantine/core";
+import Layout from "components/layout";
 import FullPage from "components/layout/FullPage";
 import type { NextPage } from "next";
 import type { BuiltInProviderType } from "next-auth/providers";
 import type { ClientSafeProvider, LiteralUnion } from "next-auth/react";
-import { getProviders, signIn, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { FcGoogle } from "react-icons/fc";
 
 type LoginProps = {
   providers: Record<
@@ -22,29 +24,24 @@ const Login: NextPage<LoginProps> = () => {
   }
 
   return (
-    <FullPage>
-      <Stack sx={{ justifyContent: "center", height: "100%" }}>
-        <Title>Login Page</Title>
-        <Button
-          color="dark"
-          variant="outline"
-          onClick={() => signIn("google")}
+    <Layout>
+      <FullPage>
+        <Stack
+          sx={{ justifyContent: "center", height: "100%" }}
+          px="xl"
         >
-          Sign In with Google
-        </Button>
-      </Stack>
-    </FullPage>
+          <Button
+            color="dark"
+            variant="outline"
+            onClick={() => signIn("google")}
+            leftIcon={<FcGoogle size={20} />}
+          >
+            Sign In with Google
+          </Button>
+        </Stack>
+      </FullPage>
+    </Layout>
   );
 };
-
-export async function getServerSideProps() {
-  const providers = await getProviders();
-
-  return {
-    props: {
-      providers,
-    },
-  };
-}
 
 export default Login;
